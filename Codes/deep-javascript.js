4011dd587166b6e7eaf5935e9c840d15de155294
YingQQQ <yingqi2008win@gmail.com>
2019-04-14 21:10:15 +0800

    关于JS继承和原型关系

--- Codes/deep-javascript.js
+++ Codes/deep-javascript.js
@@ -10,18 +10,18 @@ const objectIS = function objectIs(x, y) {
   const xIsNegative = isNegative(x);
   const yIsNegative = isNegative(y);
   if (xIsNegative || yIsNegative) {
-    return xIsNegative && yIsNegative
+    return xIsNegative && yIsNegative;
   }
   if (isNaNs(x) && isNaNs(y)) {
     return true;
   }
   return x === y;
-}
+};
 
-console.log(objectIS(0, -0) === false)
-console.log(objectIS(-0, -0) === true)
-console.log(objectIS(NaN, NaN) === true)
-console.log(objectIS('0', -0) === false)
+console.log(objectIS(0, -0) === false);
+console.log(objectIS(-0, -0) === true);
+console.log(objectIS(NaN, NaN) === true);
+console.log(objectIS("0", -0) === false);
 
 /**
  * 第二部分 隐式转换
@@ -32,14 +32,14 @@ const toSting = {
   toString() {
     return "x";
   }
-}
-console.log(`${toSting}y`)
+};
+console.log(`${toSting}y`);
 
 const toNumber = {
   valueOf() {
     return 3;
   }
-}
+};
 
 console.log(1 + toNumber);
 
@@ -62,15 +62,15 @@ console.log(3 > 2 > 1);
  * 隐式转换也有优点,但是需要理解原理,
  * 例如下面: '2' 被隐式转换成了数字2, 减少了Number('2')的代码量
  */
-console.log(Number('2') < 1);
-console.log('2' < 1);
-console.log(1 < '2');
+console.log(Number("2") < 1);
+console.log("2" < 1);
+console.log(1 < "2");
 
 /**
  * == 和 === 的区别，==比较的时候会隐式的转换类型, 而=== 不会
  */
 
- const workNumber = 42;
+const workNumber = 42;
 const workNumberArray = [42];
 
 // 因为[42]字符串化'42', 然后 42 == '42'; 隐式转换 42 == 42
@@ -78,5 +78,95 @@ const workNumberArray = [42];
 console.log(workNumber == workNumberArray); // true
 console.log(workNumber === workNumberArray); // false
 
+// 箭头函数的this问题，重点：箭头函数没有this,因此需要在上级的作用域中检索(scope)
+const workShop1 = {
+  teacher: "Tom",
+  ask: function askName(say) {
+    console.log(this.teacher, say);
+  }
+};
 
-console.log('Frank' > 'Suzy');
+// this.teacher = 'global';
+
+const workShop2 = {
+  teacher: "Tom",
+  ask: say => {
+    console.log(this.teacher, say); // 没有this, 因此需要到global scope去寻找, 可以尝试上面注释的内容global.teacher
+  }
+};
+workShop1.ask("hello world");
+workShop2.ask("hello world");
+
+// 关于继承
+// 继承可以理解为一种委托机制, Teacher的say, 通过prototype由Person实现
+// 继承就是OLOO(oject link to other object)
+
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.say = function say(something) {
+  console.log(`${this.name} ask ${something}`);
+};
+
+function Teacher(name) {
+  Person.call(this, name); // 赋予属性
+}
+
+Teacher.prototype = Object.create(Person.prototype);
+
+Teacher.prototype.ask = function ask(question) {
+  console.log(`Teacher said ${question}`);
+};
+
+const jack = new Teacher("jack");
+
+jack.say("This is prototype inheritance");
+jack.ask("What is prototype inheritance");
+
+// OLOO(oject link to other object)
+
+const PersonA = {
+  setName(name) {
+    this.name = name;
+  },
+  say(something) {
+    console.log(`${this.name} ask ${something}`);
+  }
+};
+
+const TeacherA = Object.assign(Object.create(PersonA), {
+  ask(question) {
+    console.log(`Teacher said ${question}`);
+  }
+});
+
+const EnglishTeacher = Object.create(TeacherA);
+EnglishTeacher.ask("EnglishTeacher");
+EnglishTeacher.setName("July");
+EnglishTeacher.say("EnglishTeacher");
+
+// 继承的关键是Object.create
+
+if (!Object.create) {
+  Object.create = function objectCreate(o) {
+    function F() {} // 创建一个没有副作用的空函数
+    F.prototype = o; // 把需要继承的方法赋值给原型
+    return new F();  // 实例化空函数,可以保护O对象的方法
+  };
+}
+
+
+// ES6 部分
+
+// 模板字符串
+const amount = 13.3;
+const other = 11;
+
+function formatCurrent(string, ...value) {
+  console.log(Array.isArray(string)) // true
+  console.log(`string:${string}`); // true
+  console.log(`valus => ${value}`)
+}
+
+const msg = formatCurrent`The total for your order is ${amount} and ${other}`
